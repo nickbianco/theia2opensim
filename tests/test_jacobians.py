Fix Jacobian tests

tests/test_jacobians.py
@@ -7,7 +7,6 @@ from theia2opensim.callbacks import Callback, JacobianCallback, TrackingCostCall
                                     TrackingCostJacobianCallback
 from theia2opensim.utilities import get_coordinate_indexes
 
-
 # Position Callbacks
 # ------------------
 class PositionMixin:
@@ -16,37 +15,35 @@ class PositionMixin:
         self.mobod_index = self.body.getMobilizedBodyIndex()
 
     def _get_num_inputs(self):
-        return self.state.getNQ()
+        return len(self.coordinate_indexes)
 
     def _get_num_outputs(self):
         return 3
 
     def _eval(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
+        self.apply_state(arg)
         position = self.body.getPositionInGround(self.state).to_numpy()
         return [position]
 
 
 class PositionCallback(PositionMixin, Callback):
-    def __init__(self, name, model, body_name, opts={}):
-        Callback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, body_name, opts={}):
+        Callback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_position_components(model, body_name)
 
 
 class PositionJacobianCallback(PositionMixin, JacobianCallback):
-    def __init__(self, name, model, body_name, opts={}):
-        JacobianCallback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, body_name, opts={}):
+        JacobianCallback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_position_components(model, body_name)
 
     def _jac_eval(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
-
+        self.apply_state(arg)
         matrix = osim.Matrix()
         self.matter.calcStationJacobian(self.state, self.mobod_index, osim.Vec3(0),
                                         matrix)
-        return [matrix.to_numpy()]
+        return [matrix.to_numpy()[:, self.coordinate_indexes]]
+
 
 # Position Error Callbacks
 # ------------------------
@@ -62,33 +59,31 @@ class PositionErrorMixin:
         self.reference = reference
 
     def _get_num_inputs(self):
-        return self.state.getNQ()
+        return len(self.coordinate_indexes)
 
     def _get_num_outputs(self):
         return 1
 
     def _eval(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
+        self.apply_state(arg)
         position = self.frame.getPositionInGround(self.state).to_numpy()
         error = np.square(np.linalg.norm(position - self.reference))
         return [error]
 
 
 class PositionErrorCallback(PositionErrorMixin, Callback):
-    def __init__(self, name, model, frame_path, reference, opts={}):
-        Callback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, frame_path, reference, opts={}):
+        Callback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_position_error_components(model, frame_path, reference)
 
 
 class PositionErrorJacobianCallback(PositionErrorMixin, JacobianCallback):
-    def __init__(self, name, model, frame_path, reference, opts={}):
-        JacobianCallback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, frame_path, reference, opts={}):
+        JacobianCallback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_position_error_components(model, frame_path, reference)
 
     def _jac_eval(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
+        self.apply_state(arg)
         error = self.frame.getPositionInGround(self.state)
         error[0] -= self.reference[0]
         error[1] -= self.reference[1]
@@ -98,7 +93,7 @@ class PositionErrorJacobianCallback(PositionErrorMixin, JacobianCallback):
         self.matter.multiplyByStationJacobianTranspose(self.state, self.mobod_index,
                                                        self.station, error, vec)
         J = 2.0*vec.to_numpy()
-        return [np.expand_dims(J, axis=0)]
+        return [np.expand_dims(J[self.coordinate_indexes], axis=0)]
 
 
 # Orientation Jacobians
@@ -110,14 +105,12 @@ class OrientationMixin:
         self.mobod_index = self.body.getMobilizedBodyIndex()
 
     def _get_num_inputs(self):
-        return self.state.getNQ()
+        return len(self.coordinate_indexes)
 
     def _get_num_outputs(self):
         return 4
 
-    def _calc_quaternion(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
+    def _calc_quaternion(self):
         rotation = self.body.getRotationInGround(self.state)
         quaternion = rotation.convertRotationToQuaternion()
         eps = np.array([quaternion.get(0), quaternion.get(1),
@@ -125,23 +118,24 @@ class OrientationMixin:
         return eps
 
     def _eval(self, arg):
-        q = self._calc_quaternion(arg)
-        return [q]
+        self.apply_state(arg)
+        eps = self._calc_quaternion()
+        return [eps]
 
 
 class OrientationCallback(OrientationMixin, Callback):
-    def __init__(self, name, model, body, opts={}):
-        Callback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, body, opts={}):
+        Callback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_orientation_components(model, body)
 
 
 class OrientationJacobianCallback(OrientationMixin, JacobianCallback):
-    def __init__(self, name, model, body, opts={}):
-        JacobianCallback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, body, opts={}):
+        JacobianCallback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_orientation_components(model, body)
 
-    def _calc_quaternion_jacobian(self, arg):
-        e = 0.5*self._calc_quaternion(arg)
+    def _calc_quaternion_jacobian(self):
+        e = 0.5*self._calc_quaternion()
 
         # SimbodyMatterSubsystem::calcFrameJacobian() returns a Jacobian that maps
         # generalized speeds to the spatial velocity of a frame: [omega; vdot]. So, this
@@ -168,19 +162,18 @@ class OrientationJacobianCallback(OrientationMixin, JacobianCallback):
         ])
         return jac_eps
 
-    def _calc_frame_jacobian(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
+    def _calc_frame_jacobian(self):
         jac_frame = osim.Matrix()
         self.matter.calcFrameJacobian(self.state, self.mobod_index, osim.Vec3(0),
                                       jac_frame)
         return jac_frame.to_numpy()
 
     def _jac_eval(self, arg):
-        jac_eps = self._calc_quaternion_jacobian(arg)
-        jac_frame = self._calc_frame_jacobian(arg)
+        self.apply_state(arg)
+        jac_eps = self._calc_quaternion_jacobian()
+        jac_frame = self._calc_frame_jacobian()
         jac = jac_eps.dot(jac_frame)
-        return [jac[0:4, :]]
+        return [jac[0:4, self.coordinate_indexes]]
 
 
 # Orientation Error Callbacks
@@ -196,14 +189,13 @@ class OrientationErrorMixin:
         self.reference = reference
 
     def _get_num_inputs(self):
-        return self.state.getNQ()
+        return len(self.coordinate_indexes)
 
     def _get_num_outputs(self):
         return 1
 
     def _calc_quaternion(self, arg):
-        self.state.setQ(osim.Vector.createFromMat(np.squeeze(arg[0].full())))
-        self.model.realizePosition(self.state)
+        self.apply_state(arg)
         rotation = self.frame.getRotationInGround(self.state)
         quaternion = rotation.convertRotationToQuaternion()
         eps = np.array([quaternion.get(0), quaternion.get(1),
@@ -217,14 +209,14 @@ class OrientationErrorMixin:
 
 
 class OrientationErrorCallback(OrientationErrorMixin, Callback):
-    def __init__(self, name, model, frame_path, reference, opts={}):
-        Callback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, frame_path, reference, opts={}):
+        Callback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_orientation_error_components(model, frame_path, reference)
 
 
 class OrientationErrorJacobianCallback(OrientationErrorMixin, JacobianCallback):
-    def __init__(self, name, model, frame_path, reference, opts={}):
-        JacobianCallback.__init__(self, name, model, opts)
+    def __init__(self, name, model, coordinate_indexes, frame_path, reference, opts={}):
+        JacobianCallback.__init__(self, name, model, coordinate_indexes, opts)
         self._init_orientation_error_components(model, frame_path, reference)
 
     def _calc_quaternion_jacobian(self, eps):
@@ -239,6 +231,7 @@ class OrientationErrorJacobianCallback(OrientationErrorMixin, JacobianCallback):
         return jac_eps
 
     def _jac_eval(self, arg):
+        self.apply_state(arg)
         eps = self._calc_quaternion(arg)
         jac_eps = self._calc_quaternion_jacobian(eps)
         omega = jac_eps.T.dot(self.reference)
@@ -248,15 +241,27 @@ class OrientationErrorJacobianCallback(OrientationErrorMixin, JacobianCallback):
         self.matter.multiplyByFrameJacobianTranspose(self.state, self.mobod_index,
                                                      self.station, spatial_vec, vec)
         J = -2.0*(np.dot(eps, self.reference))*vec.to_numpy()
-        return [np.expand_dims(J, axis=0)]
+        return [np.expand_dims(J[self.coordinate_indexes], axis=0)]
 
 
 # Unit tests
 # ----------
+# This needs to be generated via 'create_generic_model.py' script first.
+MODEL_FPATH = 'unscaled_generic.osim'
+FRAME_PATHS = ['/bodyset/pelvis/pelvis',
+               '/bodyset/torso/torso',
+               '/jointset/hip_r/femur_r_offset/r_thigh',
+               '/jointset/walker_knee_r/tibia_r_offset/r_shank',
+               '/jointset/ankle_r/talus_r_offset/r_foot',
+               '/jointset/mtp_r/toes_r_offset/r_toes']
+
+
 class TestPositionJacobians(unittest.TestCase):
     def test_position_jacobians(self):
-        model = osim.Model('unscaled_generic.osim')
+        model = osim.Model(MODEL_FPATH)
         state = model.initSystem()
+        coordinates_map = get_coordinate_indexes(model, skip_dependent_coordinates=True)
+        coordinate_indexes = list(coordinates_map.values())
 
         bodyset = model.getBodySet()
         for ibody in range(bodyset.getSize()):
@@ -264,11 +269,13 @@ class TestPositionJacobians(unittest.TestCase):
             body_name = body.getName()
 
             # Callback functions.
-            f_fd = PositionCallback('f_fd', model, body_name, {"enable_fd": True})
-            f_jac = PositionJacobianCallback('f_jac', model, body_name)
+            f_fd = PositionCallback('f_fd', model, coordinate_indexes, body_name,
+                                    {"enable_fd": True})
+            f_jac = PositionJacobianCallback('f_jac', model, coordinate_indexes,
+                                             body_name)
 
             # Symbolic inputs.
-            x = ca.MX.sym("x", state.getNQ())
+            x = ca.SX.sym('x', len(coordinate_indexes))
 
             # Jacobian expression graphs.
             J_fd = ca.Function('J_fd',[x],[ca.jacobian(f_fd(x), x)])
@@ -280,24 +287,21 @@ class TestPositionJacobians(unittest.TestCase):
 
 class TestPositionErrorJacobians(unittest.TestCase):
     def test_position_error_jacobians(self):
-        model = osim.Model('unscaled_generic.osim')
+        model = osim.Model(MODEL_FPATH)
         state = model.initSystem()
         reference = np.array([0.1, 0.2, 0.3])
-        frame_paths = ['/bodyset/pelvis/pelvis',
-                       '/bodyset/torso/torso',
-                       '/jointset/hip_r/femur_r_offset/r_thigh',
-                       '/jointset/walker_knee_r/tibia_r_offset/r_shank',
-                       '/jointset/ankle_r/talus_r_offset/r_foot',
-                       '/jointset/mtp_r/toes_r_offset/r_toes']
 
-        for frame_path in frame_paths:
+        coordinates_map = get_coordinate_indexes(model, skip_dependent_coordinates=True)
+        coordinate_indexes = list(coordinates_map.values())
 
-            f_fd = PositionErrorCallback('f_fd', model, frame_path, reference,
-                                        {"enable_fd": True})
-            f_jac = PositionErrorJacobianCallback('f_jac', model, frame_path, reference)
+        for frame_path in FRAME_PATHS:
+            f_fd = PositionErrorCallback('f_fd', model, coordinate_indexes, frame_path,
+                                         reference, {"enable_fd": True})
+            f_jac = PositionErrorJacobianCallback('f_jac', model, coordinate_indexes,
+                                                  frame_path, reference)
 
             # Symbolic inputs.
-            x = ca.MX.sym("x", state.getNQ())
+            x = ca.SX.sym('x', len(coordinate_indexes))
 
             # Jacobian expression graphs.
             J_fd = ca.Function('J_fd',[x],[ca.jacobian(f_fd(x), x)])
@@ -309,8 +313,10 @@ class TestPositionErrorJacobians(unittest.TestCase):
 
 class TestOrientationJacobians(unittest.TestCase):
     def test_orientation_jacobians(self):
-        model = osim.Model('unscaled_generic.osim')
+        model = osim.Model(MODEL_FPATH)
         state = model.initSystem()
+        coordinates_map = get_coordinate_indexes(model, skip_dependent_coordinates=True)
+        coordinate_indexes = list(coordinates_map.values())
 
         bodyset = model.getBodySet()
         for ibody in range(bodyset.getSize()):
@@ -318,11 +324,13 @@ class TestOrientationJacobians(unittest.TestCase):
             body_name = body.getName()
 
             # Callback functions.
-            f_fd = OrientationCallback('f_fd', model, 'pelvis', {'enable_fd': True})
-            f_jac = OrientationJacobianCallback('f_jac', model, 'pelvis')
+            f_fd = OrientationCallback('f_fd', model, coordinate_indexes, 'pelvis',
+                                       {'enable_fd': True})
+            f_jac = OrientationJacobianCallback('f_jac', model, coordinate_indexes,
+                                                'pelvis')
 
             # Symbolic inputs.
-            x = ca.MX.sym('x', state.getNQ())
+            x = ca.SX.sym('x', len(coordinate_indexes))
 
             # Jacobian expression graphs.
             J_fd = ca.Function('J_fd',[x],[ca.jacobian(f_fd(x), x)])
@@ -334,24 +342,20 @@ class TestOrientationJacobians(unittest.TestCase):
 
 class TestOrientationErrorJacobians(unittest.TestCase):
     def test_position_error_jacobians(self):
-        model = osim.Model('unscaled_generic.osim')
+        model = osim.Model(MODEL_FPATH)
         state = model.initSystem()
         reference = np.array([1.0, 0.0, 0.0, 0.0])
-        frame_paths = ['/bodyset/pelvis/pelvis',
-                       '/bodyset/torso/torso',
-                       '/jointset/hip_r/femur_r_offset/r_thigh',
-                       '/jointset/walker_knee_r/tibia_r_offset/r_shank',
-                       '/jointset/ankle_r/talus_r_offset/r_foot',
-                       '/jointset/mtp_r/toes_r_offset/r_toes']
+        coordinates_map = get_coordinate_indexes(model, skip_dependent_coordinates=True)
+        coordinate_indexes = list(coordinates_map.values())
 
-        for frame_path in frame_paths:
-            f_fd = OrientationErrorCallback('f_fd', model, frame_path, reference,
-                                        {"enable_fd": True})
-            f_jac = OrientationErrorJacobianCallback('f_jac', model, frame_path,
-                                                     reference)
+        for frame_path in FRAME_PATHS:
+            f_fd = OrientationErrorCallback('f_fd', model, coordinate_indexes,
+                                            frame_path, reference, {"enable_fd": True})
+            f_jac = OrientationErrorJacobianCallback('f_jac', model, coordinate_indexes,
+                                                     frame_path, reference)
 
             # Symbolic inputs.
-            x = ca.MX.sym("x", state.getNQ())
+            x = ca.SX.sym('x', len(coordinate_indexes))
 
             # Jacobian expression graphs.
             J_fd = ca.Function('J_fd',[x],[ca.jacobian(f_fd(x), x)])
@@ -363,28 +367,22 @@ class TestOrientationErrorJacobians(unittest.TestCase):
 
 class TestTrackingCostErrorJacobians(unittest.TestCase):
     def test_position_error_jacobians(self):
-        model = osim.Model('unscaled_generic.osim')
+        model = osim.Model(MODEL_FPATH)
         state = model.initSystem()
-        frame_paths = ['/bodyset/pelvis/pelvis',
-                       '/bodyset/torso/torso',
-                       '/jointset/hip_r/femur_r_offset/r_thigh',
-                       '/jointset/walker_knee_r/tibia_r_offset/r_shank',
-                       '/jointset/ankle_r/talus_r_offset/r_foot',
-                       '/jointset/mtp_r/toes_r_offset/r_toes']
         weights = {'position': 2.0,
                    'orientation': 0.3}
         coordinates_map = get_coordinate_indexes(model, skip_dependent_coordinates=True)
         coordinate_indexes = list(coordinates_map.values())
 
-        positions = osim.RowVectorVec3(len(frame_paths), osim.Vec3(0))
-        quaternions = osim.RowVectorQuaternion(len(frame_paths),
+        positions = osim.RowVectorVec3(len(FRAME_PATHS), osim.Vec3(0))
+        quaternions = osim.RowVectorQuaternion(len(FRAME_PATHS),
                                                osim.Quaternion(1,0,0,0))
         quaternions.setTo(osim.Quaternion(1,0,0,0))
-        f_fd = TrackingCostCallback('f_fd', model, coordinate_indexes, frame_paths,
+        f_fd = TrackingCostCallback('f_fd', model, coordinate_indexes, FRAME_PATHS,
                                     positions, quaternions, weights,
                                     {"enable_fd": True})
         f_jac = TrackingCostJacobianCallback('f_jac', model, coordinate_indexes,
-                                             frame_paths, positions, quaternions,
+                                             FRAME_PATHS, positions, quaternions,
                                              weights)
         # Symbolic inputs.
         x = ca.SX.sym('x', len(coordinate_indexes))

theia2opensim/callbacks.py
@@ -30,6 +30,14 @@ class Callback(ca.Callback, ABC):
     def eval(self, arg):
         return self._eval(arg)
 
+    def apply_state(self, arg):
+        # Apply the input coordinates to the model state and realize the system to the
+        # position stage.
+        q = np.zeros(self.state.getNQ())
+        q[self.coordinate_indexes] = np.squeeze(arg[0].full())
+        self.state.setQ(osim.Vector.createFromMat(q))
+        self.model.realizePosition(self.state)
+
     @abstractmethod
     def _get_num_inputs(self):
         pass
@@ -115,14 +123,6 @@ class TrackingCostMixin:
     def _get_num_outputs(self):
         return 1
 
-    def _apply_state(self, arg):
-        # Apply the input coordinates to the model state and realize the system to the
-        # position stage.
-        q = np.zeros(self.state.getNQ())
-        q[self.coordinate_indexes] = np.squeeze(arg[0].full())
-        self.state.setQ(osim.Vector.createFromMat(q))
-        self.model.realizePosition(self.state)
-
     def _calc_errors(self):
         position_errors = []
         orientation_errors = []
@@ -151,7 +151,7 @@ class TrackingCostMixin:
         return position_errors, orientation_errors
 
     def _eval(self, arg):
-        self._apply_state(arg)
+        self.apply_state(arg)
 
         # Compute the position and orientation errors, and return the weighted sum of
         # squared errors.
@@ -219,7 +219,7 @@ class TrackingCostJacobianCallback(TrackingCostMixin, JacobianCallback):
         return J_p + J_R
 
     def _jac_eval(self, arg):
-        self._apply_state
+        self.apply_state(arg)
         J = np.zeros((self.state.getNQ()))
         for i, frame in enumerate(self.frames):
             J_i = self._calc_frame_error_jacobian(frame, self.stations[i],